4a02e497d3408e9f4a90deec91f7385554b59a7b
Jose Barrera <mrjrbarreram@gmail.com>
2021-09-24 22:17:44 -0400

    add remove filters in UI panel, validation missing

--- Assets/Minimap/Scripts/FilterPanel.cs
+++ Assets/Minimap/Scripts/FilterPanel.cs
@@ -9,18 +9,62 @@ public class FilterPanel : MonoBehaviour
     private string attribute;
     private string operation;
     private string value;
-    private List<Filter> filters = new List<Filter>();
-    private InteractableObject[] interactableObjects;
+    private List<Filter> filters;
+    private List<InteractableObject> interactableObjects;
+    private List<GameObject> UIFilters = new List<GameObject>();
+    private int numOfActiveFilters = 1;
+    private GameObject UIContent;
 
     private void Awake() {
-        interactableObjects = GameObject.FindObjectsOfType<InteractableObject>();
+        interactableObjects = new List<InteractableObject>(GameObject.FindObjectsOfType<InteractableObject>());
+        UIContent = transform.Find("Content").gameObject;
+        int children = transform.Find("Content/Filters").gameObject.transform.childCount;
+        Debug.Log(children);
+        for (int i = 0; i < children; ++i)
+            UIFilters.Add(transform.Find("Content/Filters").gameObject.transform.GetChild(i).gameObject);
+        for (int i = 1; i < children; ++i)
+            UIFilters[i].SetActive(false);
+        UIContent.SetActive(false);
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.P)){
+            if(UIContent.activeSelf)
+                UIContent.SetActive(false);
+            else
+                UIContent.SetActive(true);
+        }
+    }
+
+    public void AddFilter(){
+        Debug.Log(UIFilters.Count);
+        Debug.Log(numOfActiveFilters);
+        if (numOfActiveFilters < UIFilters.Count){
+            UIFilters[numOfActiveFilters].SetActive(true);
+            numOfActiveFilters++;
+        }
+    }
+
+    public void RemoveFilter(){
+        Debug.Log(UIFilters.Count);
+        Debug.Log(numOfActiveFilters);
+        if (2 <= numOfActiveFilters){
+            UIFilters[numOfActiveFilters-1].SetActive(false);
+            numOfActiveFilters--;
+        }
+    }
+
+    public void ResetAppliedFilters(){
+        FilterController.DeactivateFilteredObjects(interactableObjects);
     }
 
     public void SendData(){
-        foreach (Transform child in transform.Find("Filters").gameObject.transform){
+        filters = new List<Filter>();
+        foreach (Transform child in transform.Find("Content/Filters").gameObject.transform){
             attribute = child.transform.Find("Attribute/Label").gameObject.GetComponent<TextMeshProUGUI>().text.ToString().Trim();
             operation = child.transform.Find("Operator/Label").gameObject.GetComponent<TextMeshProUGUI>().text.ToString().Trim();
             value = child.transform.Find("Value").gameObject.GetComponent<TMP_InputField>().text.Trim();
+            // Aqui Hacer Validaciones del Value
             if(Int32.TryParse(value, out int temp)){
                 value = temp.ToString();
             }
@@ -28,7 +72,7 @@ public class FilterPanel : MonoBehaviour
             filters.Add(new Filter(attribute, operation, clean_value));
         }
 
-        List<InteractableObject> filteredObjects = FilterController.ProccessFilters(new List<InteractableObject>(interactableObjects), filters);
+        List<InteractableObject> filteredObjects = FilterController.ProccessFilters(interactableObjects, filters);
         FilterController.ActivateFilteredObjects(filteredObjects);
     }
 }
